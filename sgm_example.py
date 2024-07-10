04a7048dadcfc730779a2e4977e42cb1e5a0c02c
Hai HUANG <101720804+maple367@users.noreply.github.com>
2024-07-10 10:42:34 +0800

    modified:   sgm_example.py 	modified:   utils/sgm.py

--- sgm_example.py
+++ sgm_example.py
@@ -1,11 +1,11 @@
 # %%
 import utils
+import numpy as np
 
 # %%
 data = utils.Data(r'history_res\e0d13b0f52684955b9d0fe4a6f76ce3a')
 res = data.load_res()
-C_mats = res['C_mats']
-C_mat_sum = C_mats['1D'] + C_mats['rad']+C_mats['2D']
+C_mat_sum = res['C_mat_sum']
 sgm_solver = utils.SGM(C_mat_sum, -0.06045577+0.03j, 200, 3)
 
 # %%
--- utils/sgm.py
+++ utils/sgm.py
@@ -23,18 +23,23 @@ class SGM():
         """
         if isinstance(self.resolution, int) == False:
             raise TypeError('resolution should be an integer.')
-        mesh_grid = np.arange(self.resolution+1)
+        mesh_grid = np.arange(self.resolution+2) # extent the mesh grid one more point to simplify the matrix construction
         XX, YY = np.meshgrid(mesh_grid, mesh_grid)
         XX = XX.flatten()
         YY = YY.flatten()
-        self.A = np.zeros((self.resolution*self.resolution*4, self.resolution*self.resolution*4), dtype=np.longcomplex) # TODO: ensure the size
+        self.A = np.zeros((len(YY)*4, len(XX)*4), dtype=np.longcomplex) # TODO: ensure the size
         self.M = self.A.copy()
-        for origin in range(self.resolution+1):
-            j = 4*origin
-            k = 4*origin
-            self.A[j:j+4, k:k+4] = self.C_mat_sum/2+self.grad_i
-            self.A[j:j+2, k+4*self.resolution:k+4*self.resolution+4] = self.C_mat_sum[0:2,:]/2-self.grad_i[0:2,:]
-            self.A[j+2:j+4, k+4:k+8] = self.C_mat_sum[2:4,:]/2-self.grad_i[2:4,:]
-            self.M[j:j+4, k:k+4] = np.eye(4)/2
-            self.M[j:j+2, k+4*self.resolution:k+4*self.resolution+4] = np.eye(4)[0:2,:]/2
-            self.M[j+2:j+4, k+4:k+8] = np.eye(4)[2:4,:]/2
+        for origin in range(len(XX)): # extent grid has no contribution to the matrix
+            if ((origin+1)%(self.resolution+2) !=0) and :
+                j = 4*origin
+                k = 4*origin
+                # for j, k
+                self.A[j:j+4, k:k+4] = self.C_mat_sum/2+self.grad_i
+                self.M[j:j+4, k:k+4] = np.eye(4)/2
+                # for j+1, k
+                self.A[j:j+2, k+len(mesh_grid):k+len(mesh_grid)+4] = self.C_mat_sum[0:2,:]/2-self.grad_i[0:2,:]
+                self.M[j:j+2, k+len(mesh_grid):k+len(mesh_grid)+4] = np.eye(4)[0:2,:]/2
+                # for j, k+1
+                self.A[j+2:j+4, k+4:k+8] = self.C_mat_sum[2:4,:]/2-self.grad_i[2:4,:]            
+                self.M[j+2:j+4, k+4:k+8] = np.eye(4)[2:4,:]/2
+                # if is extent grid
