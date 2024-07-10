32698af87ea71a9da62c340707498ab9a72421c3
Hai HUANG <101720804+maple367@users.noreply.github.com>
2024-07-10 09:21:39 +0800

    modified:   sgm_example.py 	modified:   utils/comsol_model/DQW-1D_20240709.mph 	deleted:    utils/comsol_model/DQW-1D_20240709.mph.lock 	modified:   utils/sgm.py

--- sgm_example.py
+++ sgm_example.py
@@ -2,13 +2,12 @@
 import utils
 
 # %%
-data = utils.Data(r'history_res\0babe404bd5748eaa98b8a4ea0d9677c')
+data = utils.Data(r'history_res\e0d13b0f52684955b9d0fe4a6f76ce3a')
 res = data.load_res()
 C_mats = res['C_mats']
 C_mat_sum = C_mats['1D'] + C_mats['rad']+C_mats['2D']
-sgm_solver = utils.SGM(C_mat_sum, -0.06045577+0.03j, 200, 7)
+sgm_solver = utils.SGM(C_mat_sum, -0.06045577+0.03j, 200, 3)
 
 # %%
-sgm_solver.run(10)
-sgm_solver.plot()
-# %%
+pass
+
--- utils/sgm.py
+++ utils/sgm.py
@@ -1,97 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.sparse.linalg import eigs
 
 class SGM():
     def __init__(self, C_mat_sum, init_eig_guess:complex, size:float, resolution:int):
         self.C_mat_sum = C_mat_sum
         self.init_eig_guess = init_eig_guess
-        self.size = size
+        self.size = size # um
         self.resolution = resolution
         self.err = 1.0
         self.tol = 1e-5
-        self._mesh_grid_()
+        self.grad_i = np.array([[-1.0j, 0.0, 0.0, 0.0],
+                                [0.0, 1.0j, 0.0, 0.0],
+                                [0.0, 0.0, -1.0j, 0.0],
+                                [0.0, 0.0, 0.0, 1.0j]])/(self.size/self.resolution)
+        self._construct_matrix_()
 
-    def _mesh_grid_(self):
-        self.dx = self.size/self.resolution
-        self.dy = self.size/self.resolution
-        c_grid = np.ones((self.resolution+2, self.resolution+2), dtype=np.complex128)
-        x_grid = np.zeros((self.resolution+2, self.resolution+1), dtype=np.complex128)
-        y_grid = np.zeros((self.resolution+1, self.resolution+2), dtype=np.complex128)
-        d_grid = np.zeros((self.resolution+1, self.resolution+1), dtype=np.complex128)
-        self.Rx_grid = x_grid.copy()
-        self.Sx_grid = x_grid.copy()
-        self.Ry_grid = y_grid.copy()
-        self.Sy_grid = y_grid.copy()
-        self.dRx_grid = d_grid.copy()
-        self.dRy_grid = d_grid.copy()
-        self.dSx_grid = d_grid.copy()
-        self.dSy_grid = d_grid.copy()
-        self.Rx_c_grid = d_grid.copy()
-        self.Sx_c_grid = d_grid.copy()
-        self.Ry_c_grid = d_grid.copy()
-        self.Sy_c_grid = d_grid.copy()
-
-    def run(self, max_iter=1000):
-        self.max_iter = max_iter
-        self.iter = 0
-        self._initializor_()
-        while (self.err > self.tol) and (self.iter <= self.max_iter):
-            self._update_()
-            self.iter += 1
-
-    def _initializor_(self):
-        self.Rx_grid[1,1] = 1+0j
-        self.Sx_grid[1,1] = 0+0j
-
-    def _update_(self):
-        # apply boundary conditions
-        self.Rx_grid[0,:] = -self.Rx_grid[1,:]
-        self.Sx_grid[-1,:] = -self.Sx_grid[-2,:]
-        self.Ry_grid[:,0] = -self.Ry_grid[:,1]
-        self.Sy_grid[:,-1] = -self.Sy_grid[:,-2]
-        # calculate d_grid
-        dRx_grid_old = np.diff(self.Rx_grid, axis=0)
-        dSx_grid_old = np.diff(self.Sx_grid, axis=0)
-        dRy_grid_old = np.diff(self.Ry_grid, axis=1)
-        dSy_grid_old = np.diff(self.Sy_grid, axis=1)
-        # update grid
-        for y in range(self.resolution+1):
-            for x in range(self.resolution+1):
-                Rx_c = (self.Rx_grid[x,y] + self.Rx_grid[x+1,y]) / 2
-                Sx_c = (self.Sx_grid[x,y] + self.Sx_grid[x+1,y]) / 2
-                Ry_c = (self.Ry_grid[x,y] + self.Ry_grid[x,y+1]) / 2
-                Sy_c = (self.Sy_grid[x,y] + self.Sy_grid[x,y+1]) / 2
-                RS_c_vec = np.array([[Rx_c], [Sx_c], [Ry_c], [Sy_c]])
-                dRx = self.dRx_grid[x,y]
-                dSx = self.dSx_grid[x,y]
-                dRy = self.dRy_grid[x,y]
-                dSy = self.dSy_grid[x,y]
-                dRS_c_vec = np.array([[dRx], [-dSx], [dRy], [-dSy]])
-                RS_c_vec = 2/(-1j*self.init_eig_guess)*(self.C_mat_sum@RS_c_vec + 1j/self.dx*dRS_c_vec)
-                self.Rx_grid[x+1,y] = RS_c_vec[0,0] - self.Rx_grid[x,y]
-                self.Sx_grid[x+1,y] = RS_c_vec[1,0] - self.Sx_grid[x,y]
-                self.Ry_grid[x,y+1] = RS_c_vec[2,0] - self.Ry_grid[x,y]
-                self.Sy_grid[x,y+1] = RS_c_vec[3,0] - self.Sy_grid[x,y]
-        for y in range(self.resolution+1):
-            for x in range(self.resolution+1):
-                self.Rx_c_grid[x,y] = (self.Rx_grid[x,y] + self.Rx_grid[x+1,y]) / 2
-                self.Sx_c_grid[x,y] = (self.Sx_grid[x,y] + self.Sx_grid[x+1,y]) / 2
-                self.Ry_c_grid[x,y] = (self.Ry_grid[x,y] + self.Ry_grid[x,y+1]) / 2
-                self.Sy_c_grid[x,y] = (self.Sy_grid[x,y] + self.Sy_grid[x,y+1]) / 2
-                RS_c_vec = np.array([[self.Rx_c_grid[x,y]], [self.Sx_c_grid[x,y]], [self.Ry_c_grid[x,y]], [self.Sy_c_grid[x,y]]])
-                dRS_c_vec = (self.init_eig_guess*RS_c_vec - self.C_mat_sum@RS_c_vec)/1j
-                self.dRx_grid[x,y] = dRS_c_vec[0,0]
-                self.dSx_grid[x,y] = -dRS_c_vec[1,0]
-                self.dRy_grid[x,y] = dRS_c_vec[2,0]
-                self.dSy_grid[x,y] = -dRS_c_vec[3,0]
-        # calculate error
-        self.err = np.max(np.abs(dRx_grid_old - self.dRx_grid)) + np.max(np.abs(dSx_grid_old - self.dSx_grid)) + np.max(np.abs(dRy_grid_old - self.dRy_grid)) + np.max(np.abs(dSy_grid_old - self.dSy_grid))
-        print(f"\riter {self.iter}: max error {self.err}         ", end="", flush=True)
-
-    def plot(self):
-        self.fig, self.ax = plt.subplots()
-        self.image = self.ax.imshow((np.abs(self.Rx_grid)**2), cmap='hot')
-        self.ax.set_title('$|R_x|^2$')
-        self.fig.colorbar(self.image)
-        plt.show()
+    def _construct_matrix_(self):
+        """
+        construct sparse matrix for the eigenvalue problem.
+        eigenvalue problem: A @ x[i] = w[i] * M @ x[i]
+        """
+        if isinstance(self.resolution, int) == False:
+            raise TypeError('resolution should be an integer.')
+        mesh_grid = np.arange(self.resolution+1)
+        XX, YY = np.meshgrid(mesh_grid, mesh_grid)
+        XX = XX.flatten()
+        YY = YY.flatten()
+        self.A = np.zeros((self.resolution*self.resolution*4, self.resolution*self.resolution*4), dtype=np.longcomplex) # TODO: ensure the size
+        self.M = self.A.copy()
+        for origin in range(self.resolution+1):
+            j = 4*origin
+            k = 4*origin
+            self.A[j:j+4, k:k+4] = self.C_mat_sum/2+self.grad_i
+            self.A[j:j+2, k+4*self.resolution:k+4*self.resolution+4] = self.C_mat_sum[0:2,:]/2-self.grad_i[0:2,:]
+            self.A[j+2:j+4, k+4:k+8] = self.C_mat_sum[2:4,:]/2-self.grad_i[2:4,:]
+            self.M[j:j+4, k:k+4] = np.eye(4)/2
+            self.M[j:j+2, k+4*self.resolution:k+4*self.resolution+4] = np.eye(4)[0:2,:]/2
+            self.M[j+2:j+4, k+4:k+8] = np.eye(4)[2:4,:]/2
