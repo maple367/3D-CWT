8bcd70f60a6db933a972a9f5390063430e98152d
Hai HUANG <101720804+maple367@users.noreply.github.com>
2025-02-26 10:09:47 +0800

    Changes to be committed: 	modified:   NN_regression.py 	modified:   mesh_data_set_3hole_lessscale.csv 	deleted:    plot.py 	deleted:    stop

--- NN_regression.py
+++ NN_regression.py
@@ -3,6 +3,32 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import torch.cuda
+from torch import nn    #导入神经网络模块
+# gpu or cpu
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f'Using {device}.')
+from torch.utils.data import DataLoader, Dataset  #数据包管理工具
+from torchvision.transforms import ToTensor  #数据转换，张量
+from sklearn.metrics import confusion_matrix, r2_score, explained_variance_score
+import seaborn as sns
+#---------------------------------------------------#
+#   set seed
+#---------------------------------------------------#
+def seed_everything(seed=233):
+    global seed_number
+    seed_number = seed
+    import random
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+seed_everything()
+
+# %%
 all_data = np.load('mesh_data_set_3hole_lessscale.npz',allow_pickle=True)['arr_0']
 # %%
 df = pd.DataFrame(all_data, columns=['eps_array', 'Q', 'SE'])
@@ -12,18 +38,10 @@ df['eps_array'] = df['eps_array'].apply(lambda x: ((np.fft.fft2(x)/1024).astype(
 df['SE'] = df['SE'].astype(np.float32)
 df['Q'] = df['Q'].astype(np.float32)
 
-train_df = df.sample(frac=0.95, random_state=233)
+train_df = df.sample(frac=0.95, random_state=seed_number)
 test_df = df.drop(train_df.index)
 # %%
-import torch.cuda
-from torch import nn    #导入神经网络模块
-from torch.utils.data import DataLoader, Dataset  #数据包管理工具
-from torchvision.transforms import ToTensor  #数据转换，张量
-
 # visualization
-from sklearn.metrics import confusion_matrix, r2_score, explained_variance_score
-import seaborn as sns
-
 def plot_confusion_matrix(y_true, y_pred, num_classes):
     cm = confusion_matrix(y_true, y_pred, labels=range(num_classes))
     plt.figure(figsize=(8, 6))
@@ -90,16 +108,16 @@ class CustomDataset(Dataset):
 
 
 # NN model
-class SimpleFC(nn.Module):
+class FC4SE(nn.Module):
     def __init__(self):
-        super(SimpleFC, self).__init__()
+        super(FC4SE, self).__init__()
         self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(2 * 32 * 32, 2048)
         self.fc2 = nn.Linear(2048, 512)
         self.fc3 = nn.Linear(512, 256)
         self.fc4 = nn.Linear(256, 64)
         self.fc_out = nn.Linear(64, 1)
-        self.dropout = nn.Dropout(0.2)
+        # self.dropout = nn.Dropout(0.2)
         # self.activation = nn.Softplus()
         self.activation = nn.PReLU()
         self.pos_embedding = nn.Parameter(torch.randn(1, 32, 32, 1))
@@ -112,9 +130,35 @@ class SimpleFC(nn.Module):
         x = self.activation(self.fc3(x))
         x = self.activation(self.fc4(x))
         x = self.fc_out(x)
-        x = torch.sigmoid(x)
+        x = torch.sigmoid(x) # enable only for SE
         return x
-    
+
+
+class FC4Q(nn.Module):
+    def __init__(self):
+        super(FC4Q, self).__init__()
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(2 * 32 * 32, 2048)
+        self.fc2 = nn.Linear(2048, 512)
+        self.fc3 = nn.Linear(512, 256)
+        self.fc4 = nn.Linear(256, 64)
+        self.fc_out = nn.Linear(64, 1)
+        # self.dropout = nn.Dropout(0.2)
+        # self.activation = nn.Softplus()
+        self.activation = nn.PReLU()
+        self.pos_embedding = nn.Parameter(torch.randn(1, 32, 32, 1))
+
+    def forward(self, x):
+        x = x + self.pos_embedding.repeat(1,1,1,2)
+        x = self.flatten(x)
+        x = self.activation(self.fc1(x))
+        x = self.activation(self.fc2(x))
+        x = self.activation(self.fc3(x))
+        x = self.activation(self.fc4(x))
+        x = self.fc_out(x)
+        return x
+
+
 # training function
 def train(dataloader, model, loss_fn, optimizer):
     num_batches = len(dataloader)
@@ -146,80 +190,80 @@ def test(dataloader, model, loss_fn):
             pred = model(x)
             pred_list += list(pred.cpu().numpy())
             test_loss += loss_fn(pred, y.unsqueeze(1)).item()
-
         test_loss /= num_batches
-    
     print(f'Test Avg loss: {test_loss}')
     # calculate scores
     print(f'R^2: {r2_score(y_list, pred_list)}')
     print(f'Explained Variance: {explained_variance_score(y_list, pred_list)}')
-    plot_distribution(y_list, pred_list)
+    # plot_distribution(y_list, pred_list)
     return test_loss
 
-# %%
-# data set
-training_data = CustomDataset(train_df['eps_array'].values, train_df['SE'].values)
-test_data = CustomDataset(test_df['eps_array'].values, test_df['SE'].values)
+def plot_learning_curve(train_losses, test_losses):
+    import matplotlib.pyplot as plt
+    num_cut = 0
+    fig, ax = plt.subplots()
+    ax.plot(train_losses[num_cut:], label='train')
+    ax.plot(test_losses[num_cut:], label='test', color='orange')
+    ax.legend()
+    ax.set_xlabel('Epoch')
+    ax.set_ylabel('Loss')
+    plt.show()
 
-# DataLoader
-
-batch_size = 64
-train_dataloader = DataLoader(training_data,batch_size=batch_size,drop_last=True)
-test_dataloader = DataLoader(test_data,batch_size=batch_size)
-
-for X,Y in train_dataloader:  # X is the input, Y is the label
-    print(f'Shape of X:{X.shape} {X.dtype}')
-    print(f'Shape of Y:{Y.shape} {Y.dtype}')
-    break
-
-# gpu or cpu
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f'Using {device}.')
-# %%
-
-model = SimpleFC().to(device)
-# loss_fn = nn.MSELoss()  # Mean Squared Error
-# loss_fn = nn.CrossEntropyLoss()
-loss_fn = nn.SmoothL1Loss(beta=0.1)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
-
-# initialize the iteration of epochs
-epochs = 100
-train_losses = []
-test_losses = []
-
-# %%
-# run the training and testing loop
-for epoch in range(epochs):
-    print(f'Epoch {epoch + 1}/{epochs}')
-    train_losses += [train(train_dataloader, model, loss_fn, optimizer)]
-    test_losses += [test(test_dataloader, model, loss_fn)]
-
-# %%
-import matplotlib.pyplot as plt
-num_cut = 0
-fig, ax = plt.subplots()
-ax.plot(train_losses[num_cut:], label='train')
-ax.plot(test_losses[num_cut:], label='test', color='orange')
-ax.legend()
-ax.set_xlabel('Epoch')
-ax.set_ylabel('Loss')
-plt.show()
-
-model.eval()
-test_loss = 0
-pred_list = []
-y_list = []
-with torch.no_grad():
-    for x, y in test_dataloader:
-        y_list += list(y.cpu().numpy())
-        x, y = x.to(device), y.to(device)
-        pred = model(x)
-        pred_list += list(pred.cpu().numpy())
-        test_loss += loss_fn(pred, y.unsqueeze(1)).item()
-pred_list = np.array(pred_list).flatten()
-y_list = np.array(y_list).flatten()
-plot_error_distribution(y_list, pred_list, cumulative=True)
 from scipy.stats import pearsonr
-print(f'Pearson Correlation: {pearsonr(y_list, pred_list)}')
+def evaluate_model(model, dataloader):
+    model.eval()
+    pred_list = []
+    y_list = []
+    with torch.no_grad():
+        for x, y in dataloader:
+            y_list += list(y.cpu().numpy())
+            x, y = x.to(device), y.to(device)
+            pred = model(x)
+            pred_list += list(pred.cpu().numpy())
+    pred_list = np.array(pred_list).flatten()
+    y_list = np.array(y_list).flatten()
+    plot_distribution(y_list, pred_list)
+    plot_error_distribution(y_list, pred_list, cumulative=True)
+    print(f'Pearson Correlation: {pearsonr(y_list, pred_list)}')
+
+def epoch_run(model, train_dataloader, test_dataloader, loss_fn, optimizer, epochs=100):
+    train_losses = []
+    test_losses = []
+    for epoch in range(epochs):
+        print(f'##### Epoch {epoch + 1}/{epochs} #####')
+        train_losses += [train(train_dataloader, model, loss_fn, optimizer)]
+        test_losses += [test(test_dataloader, model, loss_fn)]
+    plot_learning_curve(train_losses, test_losses)
+    return train_losses, 
+
+def generate_dataloader(training_data, test_data, batch_size=64):
+    train_dataloader = DataLoader(training_data, batch_size=batch_size, drop_last=True)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+#    for X,Y in train_dataloader:  # X is the input, Y is the label
+#        print(f'Shape of X:{X.shape} {X.dtype}')
+#        print(f'Shape of Y:{Y.shape} {Y.dtype}')
+#        break
+    return train_dataloader, test_dataloader
+
+def run_SE():
+    train_dataloader, test_dataloader = generate_dataloader(CustomDataset(train_df['eps_array'].values, train_df['SE'].values),
+                                                            CustomDataset(test_df['eps_array'].values, test_df['SE'].values),
+                                                            batch_size=64)
+    model = FC4SE().to(device)
+    loss_fn = nn.SmoothL1Loss(beta=0.1)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
+    train_losses, test_losses = epoch_run(model, train_dataloader, test_dataloader, loss_fn, optimizer)
+    evaluate_model(model, test_dataloader)
+
+def run_Q():
+    train_dataloader, test_dataloader = generate_dataloader(CustomDataset(train_df['eps_array'].values, np.log10(1/train_df['Q'].values)),
+                                                            CustomDataset(test_df['eps_array'].values, np.log10(1/test_df['Q'].values)),
+                                                            batch_size=64)
+    model = FC4Q().to(device)
+    loss_fn = nn.SmoothL1Loss(beta=0.1)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
+    train_losses, test_losses = epoch_run(model, train_dataloader, test_dataloader, loss_fn, optimizer)
+    evaluate_model(model, test_dataloader)
+
 # %%
+run_SE()
