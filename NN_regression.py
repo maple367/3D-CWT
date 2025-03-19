4c4367bf8583388aef686a023c9b9895b368510a
Hai HUANG <101720804+maple367@users.noreply.github.com>
2025-03-19 13:59:58 +0800

    modified:   NN_regression.py 	modified:   Q_model.pth 	modified:   SE_model.pth 	new file:   nn_opt/Q/best_network.pth 	new file:   nn_opt/SE/best_network.pth 	new file:   shape_study copy.py 	modified:   shape_study.py

--- NN_regression.py
+++ NN_regression.py
@@ -50,23 +50,25 @@ import os
 if not os.path.exists('./nn_opt'):
     os.makedirs('./nn_opt')
 
-def plot_distribution(y_true, y_pred, kde=False, prefix=''):
+def plot_distribution(y_true, y_pred, kde=False, prefix='', clf=True):
     y_true = np.array(y_true).flatten()
     y_pred = np.array(y_pred).flatten()
     plt.figure(figsize=(8, 6))
     if kde:
         sns.kdeplot(x=y_true, y=y_pred, fill=True, cbar=True, thresh=0.05)
     # Plotting the distribution of true vs predicted values
-    plt.scatter(y_true, y_pred, alpha=0.15)
-    plt.plot([min(y_true), max(y_true)], [min(y_true), max(y_true)], 'r--', label="Perfect Fit")  # Diagonal line
+    plt.scatter(y_true, y_pred, alpha=0.15, label='Test data')
+    plt.plot([min(y_true), max(y_true)], [min(y_true), max(y_true)], 'r--', label="Perfect fit")  # Diagonal line
     plt.xlabel('True Values')
     plt.ylabel('Predicted Values')
     plt.title('True vs Predicted Values')
     plt.legend()
     plt.savefig(f'./nn_opt/{prefix}_true_vs_pred.png')
-    plt.clf()
+    print(f'True vs Predicted values saved as ./nn_opt/{prefix}_true_vs_pred.png')
+    if clf:
+        plt.clf()
 
-def plot_error_distribution(y_true, y_pred, bins=50, density=True, cumulative=True, alpha=0.6, color='g', prefix='', **kwargs):
+def plot_error_distribution(y_true, y_pred, bins=50, density=True, cumulative=True, alpha=0.6, color='g', prefix='', clf=True, **kwargs):
     error = np.abs(np.array(y_true) - np.array(y_pred))
     plt.figure(figsize=(8, 6))
     plt.hist(error, bins=bins, density=density, cumulative=cumulative, alpha=alpha, color=color, **kwargs)
@@ -74,18 +76,22 @@ def plot_error_distribution(y_true, y_pred, bins=50, density=True, cumulative=Tr
     plt.ylabel('Cumulative Frequency')
     plt.title('Error Distribution')
     plt.savefig(f'./nn_opt/{prefix}_error_distribution.png')
-    plt.clf()
+    print(f'Error distribution saved as ./nn_opt/{prefix}_error_distribution.png')
+    if clf:
+        plt.clf()
 
-def plot_learning_curve(train_losses, test_losses, prefix=''):
+def plot_learning_curve(train_losses, test_losses, prefix='', clf=True,):
     num_cut = 0
     fig, ax = plt.subplots()
-    ax.plot(train_losses[num_cut:], label='train')
-    ax.plot(test_losses[num_cut:], label='test', color='orange')
+    ax.plot(train_losses[num_cut:], label='Train loss')
+    ax.plot(test_losses[num_cut:], label='Test loss', color='orange')
     ax.legend()
     ax.set_xlabel('Epoch')
     ax.set_ylabel('Loss')
     plt.savefig(f'./nn_opt/{prefix}_learning_curve.png')
-    plt.clf()
+    print(f'Learning curve saved as ./nn_opt/{prefix}_learning_curve.png')
+    if clf:
+        plt.clf()
 
 class CustomDataset(Dataset):
     def __init__(self, data, labels):
@@ -104,11 +110,11 @@ class CustomDataset(Dataset):
             flited = []
             for i in range(32):
                 for j in range(32):
-                    if i < 16:
+                    if i <= 16:
                         if j >= 32 - i:
                             flited.append(data[i][j])
                     else:
-                        if j >= 31 - i:
+                        if j >= 33 - i:
                             flited.append(data[i][j])
             data = np.array(flited)
             real_part = np.real(data)
@@ -136,13 +142,13 @@ class FC4SE(nn.Module):
     def __init__(self):
         super(FC4SE, self).__init__()
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(32 * 32, 1024)
+        self.fc1 = nn.Linear(962, 1024)
         self.fc2 = nn.Linear(1024, 512)
         self.fc3 = nn.Linear(512, 256)
         self.fc4 = nn.Linear(256, 64)
         self.fc_out = nn.Linear(64, 1)
         self.activation = nn.PReLU()
-        self.pos_embedding = nn.Parameter(torch.randn(1, 512, 1))
+        self.pos_embedding = nn.Parameter(torch.randn(1, 481, 1))
 
     def forward(self, x):
         x = x + self.pos_embedding.repeat(1,1,2)
@@ -160,13 +166,13 @@ class FC4Q(nn.Module):
     def __init__(self):
         super(FC4Q, self).__init__()
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(32 * 32, 1024)
+        self.fc1 = nn.Linear(962, 1024)
         self.fc2 = nn.Linear(1024, 512)
         self.fc3 = nn.Linear(512, 256)
         self.fc4 = nn.Linear(256, 64)
         self.fc_out = nn.Linear(64, 1)
         self.activation = nn.PReLU()
-        self.pos_embedding = nn.Parameter(torch.randn(1, 512, 1))
+        self.pos_embedding = nn.Parameter(torch.randn(1, 481, 1))
 
     def forward(self, x):
         x = x + self.pos_embedding.repeat(1,1,2)
@@ -179,7 +185,55 @@ class FC4Q(nn.Module):
         return x
 
 
-def evaluate_model(model, dataloader, mark=''):
+class EarlyStopping:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+    def __init__(self, save_path, patience=5, verbose=False, delta=0):
+        """
+        Args:
+            save_path : 模型保存文件夹
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement. 
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
+        """
+        self.save_path = save_path
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+
+    def __call__(self, val_loss, model):
+
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        '''Saves model when validation loss decrease.'''
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        path = os.path.join(self.save_path, 'best_network.pth')
+        torch.save(model.state_dict(), path)	# 这里会存储迄今最优模型的参数
+        self.val_loss_min = val_loss
+
+
+def evaluate_model(model, dataloader, mark='',clf=True):
     model.eval()
     pred_list = []
     y_list = []
@@ -191,8 +245,8 @@ def evaluate_model(model, dataloader, mark=''):
             pred_list += list(pred.cpu().numpy())
     pred_list = np.array(pred_list).flatten()
     y_list = np.array(y_list).flatten()
-    plot_distribution(y_list, pred_list, prefix=mark)
-    plot_error_distribution(y_list, pred_list, cumulative=True, prefix=mark)
+    plot_distribution(y_list, pred_list, prefix=mark, clf=clf)
+    plot_error_distribution(y_list, pred_list, cumulative=True, prefix=mark, clf=clf)
     r2 = r2_score(y_list, pred_list)
     pearson_r = pearsonr(y_list, pred_list)
     print(f'R^2: {r2}')
@@ -241,13 +295,15 @@ class Objective:
     def _run_SE_(self, beta=0.1, lr=0.0005, trial=None):
         c_time = time.strftime('%y-%m-%d_%H-%M-%S',time.localtime(time.time()))
         model = FC4SE().to(device)
+        self.early_stopping = EarlyStopping('./nn_opt/SE')
         r2 = self._run_learn_(model, self.train_dataloader, self.test_dataloader, beta, lr, trial=trial, mark=f'SE_{c_time}')
         return r2
     
     def _run_Q_(self, beta=0.1, lr=0.0005, trial=None):
         c_time = time.strftime('%y-%m-%d_%H-%M-%S',time.localtime(time.time()))
         model = FC4Q().to(device)
-        r2 = self._run_learn_(model, self.train_dataloader, self.test_dataloader, beta, lr, trial=trial, mark='Q_{c_time}')
+        self.early_stopping = EarlyStopping('./nn_opt/Q')
+        r2 = self._run_learn_(model, self.train_dataloader, self.test_dataloader, beta, lr, trial=trial, mark=f'Q_{c_time}')
         return r2
     
     def _run_learn_(self, model, train_dataloader, test_dataloader, beta, lr, trial=None, mark=''):
@@ -255,8 +311,8 @@ class Objective:
         self.model = model
         loss_fn = nn.SmoothL1Loss(beta=beta)
         optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-        train_losses, test_losses = self._epoch_run_(model, train_dataloader, test_dataloader, loss_fn, optimizer, trial=trial)
-        plot_learning_curve(train_losses, test_losses, prefix=mark)
+        self.train_losses, self.test_losses = self._epoch_run_(model, train_dataloader, test_dataloader, loss_fn, optimizer, trial=trial)
+        plot_learning_curve(self.train_losses, self.test_losses, prefix=mark)
         r2 = evaluate_model(model, test_dataloader, mark=mark)
         return r2
         
@@ -267,6 +323,12 @@ class Objective:
             print(f'##### Epoch {epoch + 1}/{epochs} #####')
             train_losses += [self._train_(train_dataloader, model, loss_fn, optimizer)]
             test_losses += [self._test_(test_dataloader, model, loss_fn, trial, epoch)]
+                # 早停止
+            self.early_stopping(test_losses[-1], model)
+            #达到早停止条件时，early_stop会被置为True
+            if self.early_stopping.early_stop:
+                print("Early stopping")
+                break #跳出迭代，结束训练
         return train_losses, test_losses
     
     def _train_(self, dataloader, model, loss_fn, optimizer):
@@ -311,29 +373,102 @@ class Objective:
                 raise optuna.TrialPruned()
         return test_loss
 
+# %%
 # If you just want to run the training process, you can use the following code.
 # r2 = Objective('SE').run() # or Objective('Q').run()
 # print(f'R^2: {r2}')
 
 # Execute an optimization by using an `Objective` instance.
-sampler = optuna.samplers.TPESampler(seed=seed_number)
-study = optuna.create_study(sampler=sampler, direction='maximize')
-objective = Objective('SE')
-study.optimize(objective, n_trials=500, n_jobs=1)
+# sampler = optuna.samplers.TPESampler(seed=seed_number)
+# study = optuna.create_study(sampler=sampler, direction='maximize')
+# objective = Objective('Q')
+# study.optimize(objective, n_trials=200, n_jobs=1)
 '''
 SE
-trial: beta=0.09178184385648053, lr=2.1059675996980374e-05
-R^2: 0.5606043934822083
-Pearson Correlation: PearsonRResult(statistic=0.7690863364241467, pvalue=0.0)
+beta=0.12, lr=2e-05
+R^2: 0.6100635528564453
 '''
 # objective_SE = Objective('SE')
-# objective_SE.run(beta=0.20792354901416032, lr=0.00013650540364085198)
-# torch.save(objective_SE.model, 'SE_model.pth')
+# r2_ls = []
+# for beta in np.linspace(0.05, 0.15, 11):
+#     for lr in np.linspace(1e-5, 1e-4, 10):
+#         r2 = objective_SE.run(beta=beta, lr=lr)
+#         r2_ls.append(r2)
+#         if r2 >= max(r2_ls):
+#             print('-------------------')
+#             print(f'beta={beta}, lr={lr}')
+#             print(f'R^2: {r2}')
+#             torch.save(objective_SE.model.state_dict(), 'SE_model.pth')
+#             np.save('SE_model.class', objective_SE)
+#             print('-------------------')
 '''
-trial: beta=0.10824711361774697, lr=6.526571864860588e-05
-R^2: 0.7575156092643738
-Pearson Correlation: PearsonRResult(statistic=0.8733898155500965, pvalue=0.0)
+Q
+beta=0.020000000000000004, lr=0.00011999999999999999
+R^2: 0.7778209447860718
 '''
 # objective_Q = Objective('Q')
-# objective_Q.run(beta=0.16523405042720546, lr=0.00012158075688545713)
-# torch.save(objective_Q.model, 'Q_model.pth')
+# r2_ls = []
+# for beta in np.linspace(0.01, 0.1, 10):
+#     for lr in np.linspace(5e-5, 15e-5, 11):
+#         r2 = objective_Q.run(beta=beta, lr=lr)
+#         r2_ls.append(r2)
+#         if r2 >= max(r2_ls):
+#             print('-------------------')
+#             print(f'beta={beta}, lr={lr}')
+#             print(f'R^2: {r2}')
+#             torch.save(objective_Q.model.state_dict(), 'Q_model.pth')
+#             np.save('Q_model.class', objective_Q)
+#             print('-------------------')
+
+# %%
+# plot results
+SE_model = FC4SE().to(device)
+SE_model.load_state_dict(torch.load('SE_model.pth'))
+SE_model.eval()
+Q_model = FC4Q().to(device)
+Q_model.load_state_dict(torch.load('Q_model.pth'))
+Q_model.eval()
+SE_data = CustomDataset(test_df['eps_array'].values, test_df['SE'].values)
+Q_data = CustomDataset(test_df['eps_array'].values, np.log10(test_df['Q'].values))
+SE_dataloader = DataLoader(SE_data, batch_size=64)
+Q_dataloader = DataLoader(Q_data, batch_size=64)
+# evaluate_model(SE_model, SE_dataloader, mark='SE', clf=False)
+# evaluate_model(Q_model, Q_dataloader, mark='Q')
+
+# %%
+pred_list = []
+y_list = []
+with torch.no_grad():
+    for x, y in SE_dataloader:
+        y_list += list(y.cpu().numpy())
+        x, y = x.to(device), y.to(device)
+        pred = SE_model(x)
+        pred_list += list(pred.cpu().numpy())
+pred_list = np.array(pred_list).flatten()
+y_list = np.array(y_list).flatten()
+rmse = np.sqrt(np.mean((y_list - pred_list)**2))
+print(f'RMSE: {rmse}')
+plot_distribution(y_list, pred_list, prefix='SE', clf=False)
+plt.title('')
+plt.xlabel('$SEE_{CWT}$')
+plt.ylabel('$SEE_{NN}$')
+plt.tight_layout()
+plt.show()
+plot_error_distribution(y_list, pred_list, cumulative=True, prefix='SE', clf=False)
+plt.title('')
+plt.xlabel('Error ($\\left|SEE_{CWT}-SEE_{NN}\\right|$)')
+plt.ylabel('Cumulative Frequency')
+plt.tight_layout()
+plt.show()
+# %%
+objective_SE = np.load('SE_model.class.npy', allow_pickle=True).item()
+objective_Q = np.load('Q_model.class.npy', allow_pickle=True).item()
+
+# %%
+plot_learning_curve(objective_SE.train_losses, objective_SE.test_losses, clf=False)
+plt.title('')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.tight_layout()
+plt.show()
+# %%
--- shape_study.py
+++ shape_study.py
@@ -1,38 +1,16 @@
-import shap
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-def seed_everything(seed=233):
-    global seed_number
-    seed_number = seed
-    import random
-    random.seed(seed)
-    np.random.seed(seed)
-seed_everything()
-
-
-# def f(X):
-#     return np.sin(X[:,0]) + np.abs(X[:,1]) + X[:,2]**2 + X[:,3] + X[:,4]**3 + X[:,5]**4
-# X = (np.random.rand(100, 13)-0.5)*3
-# y = f(X)
-# # explainer = shap.KernelExplainer(model=f, data=X, link="identity")
-# explainer = shap.explainers.Exact(f, X)
-# X = pd.DataFrame(X, columns=[fr'$\xi_{i}$' for i in range(X.shape[1])])
-# shap_values = explainer(X)
-# shap.plots.violin(shap_values, plot_type="layered_violin", show=False, plot_size=1.0, max_display=10)
-# plt.show()
-
 # %%
 '''加载数据'''
+import time
 import numpy as np
 import pandas as pd
 from scipy.stats import pearsonr
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
+rcParams['font.family'] = 'Times New Roman'
 import torch.cuda
 from torch import nn    #导入神经网络模块
 # gpu or cpu
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = 'cpu'
 print(f'Using {device}.')
 from torch.utils.data import DataLoader, Dataset  #数据包管理工具
 from torchvision.transforms import ToTensor  #数据转换，张量
@@ -56,11 +34,11 @@ seed_everything()
 
 # %%
 all_data = np.load('mesh_data_set_3hole_lessscale.npz',allow_pickle=True)['arr_0']
-
+# %%
 df = pd.DataFrame(all_data, columns=['eps_array', 'Q', 'SE'])
 
 df = df[(df['SE'] <= 1.0) & (df['SE'] >= 0.0) & (df['Q'] >= 0.0)]
-df['eps_array'] = df['eps_array'].apply(lambda x: ((np.fft.fft2(x)/1024).astype(np.complex64)))
+df['eps_array'] = df['eps_array'].apply(lambda x: ((np.fft.fftshift(np.fft.fft2(x)/1024)).astype(np.complex64)))
 df['SE'] = df['SE'].astype(np.float32)
 df['Q'] = df['Q'].astype(np.float32)
 
@@ -68,31 +46,6 @@ train_df = df.sample(frac=0.95, random_state=seed_number)
 test_df = df.drop(train_df.index)
 
 # %%
-class FC4SE(nn.Module):
-    def __init__(self):
-        super(FC4SE, self).__init__()
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(2 * 32 * 32, 2048)
-        self.fc2 = nn.Linear(2048, 512)
-        self.fc3 = nn.Linear(512, 256)
-        self.fc4 = nn.Linear(256, 64)
-        self.fc_out = nn.Linear(64, 1)
-        # self.dropout = nn.Dropout(0.2)
-        # self.activation = nn.Softplus()
-        self.activation = nn.PReLU()
-        self.pos_embedding = nn.Parameter(torch.randn(1, 32, 32, 1))
-
-    def forward(self, x):
-        x = x + self.pos_embedding.repeat(1,1,1,2)
-        x = self.flatten(x)
-        x = self.activation(self.fc1(x))
-        x = self.activation(self.fc2(x))
-        x = self.activation(self.fc3(x))
-        x = self.activation(self.fc4(x))
-        x = self.fc_out(x)
-        x = torch.sigmoid(x) # enable only for SE
-        return x
-
 class CustomDataset(Dataset):
     def __init__(self, data, labels):
         """
@@ -100,8 +53,27 @@ class CustomDataset(Dataset):
         :param data: input data (numpy or other format)
         :param labels: label data
         """
-        self.data = data
+        self._data_ = data
         self.labels = labels
+        self._process_data()
+        
+    def _process_data(self):
+        self.data = []
+        for data in self._data_:
+            flited = []
+            for i in range(32):
+                for j in range(32):
+                    if i <= 16:
+                        if j >= 32 - i:
+                            flited.append(data[i][j])
+                    else:
+                        if j >= 33 - i:
+                            flited.append(data[i][j])
+            data = np.array(flited)
+            real_part = np.real(data)
+            imaginary_part = np.imag(data)
+            data = np.stack((real_part, imaginary_part), axis=-1)
+            self.data.append(data)
     
     def __len__(self):
         """
@@ -114,52 +86,163 @@ class CustomDataset(Dataset):
         return the sample and label at the given index
         :param idx: index of the sample
         """
-
         sample = self.data[idx]
-        real_part = np.real(sample)
-        imaginary_part = np.imag(sample)
-        sample = np.stack((real_part, imaginary_part), axis=-1)
         label = self.labels[idx]
-        
         return sample, label
 
-test_data = CustomDataset(test_df['eps_array'].values, test_df['SE'].values)
-test_dataloader = DataLoader(test_data, batch_size=64)
-model = torch.load('SE_model.pth', weights_only=False)
-model.to('cpu')
-model.eval()
+# NN model
+class FC4SE(nn.Module):
+    def __init__(self):
+        super(FC4SE, self).__init__()
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(962, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 256)
+        self.fc4 = nn.Linear(256, 64)
+        self.fc_out = nn.Linear(64, 1)
+        self.activation = nn.PReLU()
+        self.pos_embedding = nn.Parameter(torch.randn(1, 481, 1))
+
+    def forward(self, x):
+        x = x + self.pos_embedding.repeat(1,1,2)
+        x = self.flatten(x)
+        x = self.activation(self.fc1(x))
+        x = self.activation(self.fc2(x))
+        x = self.activation(self.fc3(x))
+        x = self.activation(self.fc4(x))
+        x = self.fc_out(x)
+        x = torch.sigmoid(x) # enable only for SE
+        return x
+
+
+class FC4Q(nn.Module):
+    def __init__(self):
+        super(FC4Q, self).__init__()
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(962, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 256)
+        self.fc4 = nn.Linear(256, 64)
+        self.fc_out = nn.Linear(64, 1)
+        self.activation = nn.PReLU()
+        self.pos_embedding = nn.Parameter(torch.randn(1, 481, 1))
+
+    def forward(self, x):
+        x = x + self.pos_embedding.repeat(1,1,2)
+        x = self.flatten(x)
+        x = self.activation(self.fc1(x))
+        x = self.activation(self.fc2(x))
+        x = self.activation(self.fc3(x))
+        x = self.activation(self.fc4(x))
+        x = self.fc_out(x)
+        return x
+
+
+class EarlyStopping:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+    def __init__(self, save_path, patience=5, verbose=False, delta=0):
+        """
+        Args:
+            save_path : 模型保存文件夹
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement. 
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
+        """
+        self.save_path = save_path
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+
+    def __call__(self, val_loss, model):
+
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        '''Saves model when validation loss decrease.'''
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        path = os.path.join(self.save_path, 'best_network.pth')
+        torch.save(model.state_dict(), path)	# 这里会存储迄今最优模型的参数
+        self.val_loss_min = val_loss
+
+
+SE_model = FC4SE().to(device)
+SE_model.load_state_dict(torch.load('SE_model.pth'))
+SE_model.eval()
+Q_model = FC4Q().to(device)
+Q_model.load_state_dict(torch.load('Q_model.pth'))
+Q_model.eval()
+SE_data = CustomDataset(test_df['eps_array'].values, test_df['SE'].values)
+Q_data = CustomDataset(test_df['eps_array'].values, np.log10(test_df['Q'].values))
+SE_dataloader = DataLoader(SE_data, batch_size=1)
+Q_dataloader = DataLoader(Q_data, batch_size=1)
 
 # %%
+model = SE_model
+test_data = SE_dataloader
 def f(x):
     with torch.no_grad():
-        x = torch.tensor(x).reshape(-1,32,32,2)
+        x = torch.tensor(x).reshape(-1,481,2)
         pred = model(x)
         y = pred.numpy()
     return y.flatten()
 
 X = []
+Y = []
 for x, y in test_data:
     X.append(x.flatten())
+    Y.append(y.item())
 X = np.array(X)
+Y = np.array(Y)
+# %%
 import shap
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 # X = shap.kmeans(X, 1000)
-X = X[:10]
+X = shap.sample(X, 500, random_state=seed_number)
 explainer = shap.KernelExplainer(model=f, data=X, link="identity")
 columns_name = []
 for i in range(32):
     for j in range(32):
-        if i > 16:
-            i = i-32
-        if j > 16:
-            j = j-32
-        columns_name.append(rf'$\xi_{{{i},{j}}}^R$')
-        columns_name.append(rf'$\xi_{{{i},{j}}}^I$')
+        if i <= 16:
+            if j >= 32 - i:
+                columns_name.append(rf'$\xi_{{{i-16},{j-16}}}^R$')
+                columns_name.append(rf'$\xi_{{{i-16},{j-16}}}^I$')
+        else:
+            if j >= 33 - i:
+                columns_name.append(rf'$\xi_{{{i-16},{j-16}}}^R$')
+                columns_name.append(rf'$\xi_{{{i-16},{j-16}}}^I$')
 X = pd.DataFrame(X, columns=columns_name)
 shap_values = explainer(X)
 # %%
-shap.plots.violin(shap_values, plot_type="layered_violin", show=False, plot_size=1.0, max_display=10)
+# np.savez('shap_values_SE.npz', shap_values=shap_values)
+plt.figure(dpi=300)
+shap.plots.violin(shap_values, plot_type="layered_violin", show=False, max_display=15)
+ax = plt.gca()
+collections = ax.collections # List of all collections (e.g., scatter points)
+for c in collections:
+    c.set_edgecolor('none') # Set edge color for each collection
+plt.xlabel('SHAP value of $SEE$')
 plt.show()
 # %%
